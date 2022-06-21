e41076b8d40f6638b44ef30528951fb4ad2e2e18
Junho Yeo <i@junho.io>
2022-06-21 19:49:31 +0900

    Initial commit

---
+++ frontend/src/Home/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HomePage = () => {
+  return <Container />;
+};
+
+export default HomePage;
+
+const Container = styled.div``;
