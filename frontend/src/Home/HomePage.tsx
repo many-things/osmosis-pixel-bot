1873b5c33a1edcf9b237edb6956f5dfbafb9f07a
Junho Yeo <i@junho.io>
2022-06-22 15:41:31 +0900

    Add error handling in frontend getting grant

--- frontend/src/Home/HomePage.tsx
+++ frontend/src/Home/HomePage.tsx
@@ -14,7 +14,7 @@ import { SendAuthorization } from 'cosmjs-types/cosmos/bank/v1beta1/authz';
 import { TxRaw } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
 import { Timestamp } from 'cosmjs-types/google/protobuf/timestamp';
 import dayjs from 'dayjs';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 
 const defaultRegistryTypes: ReadonlyArray<[string, GeneratedType]> = [
@@ -31,107 +31,132 @@ declare global {
 }
 
 const HomePage = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
   const onClick = useCallback(async () => {
     if (typeof window.keplr === 'undefined') {
       window.alert('Please install keplr extension');
       return;
     }
 
-    const chainId = 'osmosis-1';
-    await window.keplr.enable(chainId);
+    setLoading(true);
+    try {
+      const chainId = 'osmosis-1';
+      await window.keplr.enable(chainId);
 
-    const offlineSigner = window.keplr.getOfflineSigner(chainId);
+      const offlineSigner = window.keplr.getOfflineSigner(chainId);
 
-    const stargateClient = await SigningStargateClient.connectWithSigner(
-      'https://osmosis-1--rpc--full.datahub.figment.io/apikey/1d501057297ffd7db2a343c2d3daf459',
-      offlineSigner,
-    );
+      const stargateClient = await SigningStargateClient.connectWithSigner(
+        'https://osmosis-1--rpc--full.datahub.figment.io/apikey/1d501057297ffd7db2a343c2d3daf459',
+        offlineSigner,
+      );
 
-    const accounts = await offlineSigner.getAccounts();
-    const firstAccount = accounts[0];
+      const accounts = await offlineSigner.getAccounts();
+      const firstAccount = accounts[0];
 
-    const pubkey = encodePubkey(encodeSecp256k1Pubkey(firstAccount.pubkey));
-    const { sequence, accountNumber } = await stargateClient.getSequence(
-      firstAccount.address,
-    );
+      const pubkey = encodePubkey(encodeSecp256k1Pubkey(firstAccount.pubkey));
+      const { sequence, accountNumber } = await stargateClient.getSequence(
+        firstAccount.address,
+      );
 
-    const grantMsg = {
-      typeUrl: '/cosmos.authz.v1beta1.MsgGrant',
-      value: {
-        granter: firstAccount.address,
-        grantee: 'osmo15zysaya5j34vy2cqd7y9q8m3drjpy0d2lvmkpa',
-        grant: {
-          authorization: {
-            typeUrl: '/cosmos.bank.v1beta1.SendAuthorization',
-            value: SendAuthorization.encode(
-              SendAuthorization.fromPartial({
-                spendLimit: [
-                  {
-                    denom: 'uosmo',
-                    amount: (1 * 10000).toString(),
-                  },
-                ],
-              }),
-            ).finish(),
+      const grantMsg = {
+        typeUrl: '/cosmos.authz.v1beta1.MsgGrant',
+        value: {
+          granter: firstAccount.address,
+          grantee: 'osmo15zysaya5j34vy2cqd7y9q8m3drjpy0d2lvmkpa',
+          grant: {
+            authorization: {
+              typeUrl: '/cosmos.bank.v1beta1.SendAuthorization',
+              value: SendAuthorization.encode(
+                SendAuthorization.fromPartial({
+                  spendLimit: [
+                    {
+                      denom: 'uosmo',
+                      amount: (1 * 10000).toString(),
+                    },
+                  ],
+                }),
+              ).finish(),
+            },
+            expiration: Timestamp.fromPartial({
+              seconds: Math.floor(
+                dayjs(dayjs().add(1, 'month')).valueOf() / 1000,
+              ),
+              nanos: 0,
+            }),
           },
-          expiration: Timestamp.fromPartial({
-            seconds: Math.floor(
-              dayjs(dayjs().add(1, 'month')).valueOf() / 1000,
-            ),
-            nanos: 0,
-          }),
         },
-      },
-    };
+      };
 
-    const txBodyEncodeObject = {
-      typeUrl: '/cosmos.tx.v1beta1.TxBody',
-      value: {
-        messages: [grantMsg],
-        memo: 'Grant for MANYTHINGS',
-      },
-    };
-    const txBodyBytes = registry.encode(txBodyEncodeObject);
+      const txBodyEncodeObject = {
+        typeUrl: '/cosmos.tx.v1beta1.TxBody',
+        value: {
+          messages: [grantMsg],
+          memo: 'Grant for MANYTHINGS',
+        },
+      };
+      const txBodyBytes = registry.encode(txBodyEncodeObject);
 
-    const signDoc = makeSignDoc(
-      txBodyBytes,
-      makeAuthInfoBytes(
-        [{ pubkey, sequence }],
-        [{ denom: 'uosmo', amount: '0' }],
-        80000,
-      ),
-      chainId,
-      accountNumber,
-    );
+      const signDoc = makeSignDoc(
+        txBodyBytes,
+        makeAuthInfoBytes(
+          [{ pubkey, sequence }],
+          [{ denom: 'uosmo', amount: '0' }],
+          80000,
+        ),
+        chainId,
+        accountNumber,
+      );
 
-    const { signed, signature } = await window.keplr.signDirect(
-      chainId,
-      firstAccount.address,
-      signDoc,
-    );
-    const txRaw = TxRaw.fromPartial({
-      bodyBytes: signed.bodyBytes,
-      authInfoBytes: signed.authInfoBytes,
-      signatures: [new Uint8Array(Buffer.from(signature.signature, 'base64'))],
-    });
+      const { signed, signature } = await window.keplr.signDirect(
+        chainId,
+        firstAccount.address,
+        signDoc,
+      );
+      const txRaw = TxRaw.fromPartial({
+        bodyBytes: signed.bodyBytes,
+        authInfoBytes: signed.authInfoBytes,
+        signatures: [
+          new Uint8Array(Buffer.from(signature.signature, 'base64')),
+        ],
+      });
 
-    const tx = TxRaw.encode(txRaw).finish();
-    console.log(tx);
-    console.log(decodeTxRaw(tx));
-    const encodedTxHash = await window.keplr.sendTx(
-      chainId,
-      tx,
-      BroadcastMode.Block,
-    );
-    const txHash = Buffer.from(encodedTxHash).toString('hex').toUpperCase();
-    window.alert(JSON.stringify(txHash));
+      const tx = TxRaw.encode(txRaw).finish();
+      const encodedTxHash = await window.keplr.sendTx(
+        chainId,
+        tx,
+        BroadcastMode.Block,
+      );
+      const txHash = Buffer.from(encodedTxHash).toString('hex').toUpperCase();
+      setLoading(false);
+
+      window.alert(`Success! Transaction: ${txHash}`);
+    } catch (e) {
+      console.error(e);
+      setLoading(false);
+
+      if (e.message.toLowerCase() === 'request rejected') {
+        return;
+      }
+
+      if (e.message.includes('500')) {
+        window.alert('Network may be congested. Please try again.');
+      } else {
+        window.alert(e.message);
+      }
+    }
   }, []);
 
   return (
     <Container>
       <ManythingsLogo src="/assets/manythings.png" />
       <Description>0.01 OSMO = 10,000 pixels</Description>
-      <Button onClick={onClick}>Grant Send Authorization</Button>
+      {!loading && <Button onClick={onClick}>Grant Send Authorization</Button>}
+      {loading && (
+        <Button onClick={() => {}} style={{ cursor: 'progress' }}>
+          Loading...
+        </Button>
+      )}
     </Container>
   );
 };
